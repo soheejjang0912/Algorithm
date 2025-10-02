b3dd4d9d5666f0331fd41fee638e6add6c953133
장소희 <66730047+soheejjang0912@users.noreply.github.com>
2025-10-02 11:43:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,11 @@
+def solution(cards1, cards2, goal):
+    idx1, idx2 = 0, 0  # 각 카드 뭉치의 현재 인덱스
+
+    for word in goal:
+        if idx1 < len(cards1) and cards1[idx1] == word:
+            idx1 += 1
+        elif idx2 < len(cards2) and cards2[idx2] == word:
+            idx2 += 1
+        else:
+            return "No"
+    return "Yes"
