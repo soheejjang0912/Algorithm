4fb44e2a84f623bf7a42e314163a2a1158c098f3
장소희 <66730047+soheejjang0912@users.noreply.github.com>
2025-11-23 14:43:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 16.23 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 11.61 ms
+메모리: 9.29 MB, 시간: 16.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 23일 14:20:10
+2025년 11월 23일 14:43:27
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,39 @@
-from collections import deque
 
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
 def solution(maps):
-    n = len(maps) # 행 개수
-    m = len(maps[0]) # 열 개수
-    
-    # 방문 여부 저장
-    visited = [[False]*m for _ in range(n)]
-    
-    # bfs
-    q = deque()
-    q.append((0, 0, 1)) # 행, 열, 거리
-    visited[0][0] = True
-    
-    # 상하좌우
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    while q:
-        x, y, dist = q.popleft()
-        
-        if x == n-1 and y == m-1:
-            return dist
-        
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < m:
-                if maps[nx][ny] == 1 and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    q.append((nx, ny, dist+1))
-        
-        
+    stack = [(0,0, 1)]
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
+
+    while stack:
+        x, y, d= stack.pop(0)
+        for q in range(4):
+            ny = y + dy[q]
+            nx = x + dx[q]
+            if ny <0 or nx <0 or ny >= len(maps) or nx >= len(maps[0]):
+                continue
+            else:
+                if maps[ny][nx] ==1 or maps[ny][nx] > d+1:
+                    maps[ny][nx] =d+1 
+                    if ny == len(maps) -1 and nx == len(maps[0])-1:
+                        return d+1
+                    stack.append((nx, ny, d+1))
     return -1
