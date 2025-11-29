96b42087077a89d68ad0554b6eff4fb75e822dcf
장소희 <66730047+soheejjang0912@users.noreply.github.com>
2025-11-29 23:29:33 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,27 @@
+WITH G AS (
+    SELECT 
+        EMP_NO,
+        SUM(SCORE) AS SCORE
+    FROM HR_GRADE
+    WHERE YEAR = 2022
+    GROUP BY EMP_NO
+),
+RANKED AS (
+    SELECT 
+        G.SCORE,
+        E.EMP_NO,
+        E.EMP_NAME,
+        E.POSITION,
+        E.EMAIL,
+        RANK() OVER (ORDER BY G.SCORE DESC) AS RN
+    FROM G
+    JOIN HR_EMPLOYEES E ON G.EMP_NO = E.EMP_NO
+)
+SELECT 
+    SCORE,
+    EMP_NO,
+    EMP_NAME,
+    POSITION,
+    EMAIL
+FROM RANKED
+WHERE RN = 1;
